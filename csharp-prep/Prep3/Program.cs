028509229840235620040a8fe9515f33d22d28bf
berni.niko <berni.niko@hotmail.com>
2024-04-25 04:42:23 +1200

    finished prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random random = new Random();
+        int magicNumber = random.Next(1, 101); // Generate a random number between 1 and 100
+
+        Console.WriteLine("Welcome to Guess My Number!");
+
+        int guess = 0; // Initialize the guess variable
+        while (guess != magicNumber) // Loop until the correct guess
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
