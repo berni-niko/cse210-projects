c54e1bfb06153a2a7880c9d866b99080f8c98fe1
berni.niko <berni.niko@hotmail.com>
2024-04-25 05:10:33 +1200

    updated

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,35 +1,35 @@
 using System;
 
-public class Program
+class Program
 {
-    public static void DisplayWelcome()
+    static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the program!");
     }
 
-    public static string PromptUserName()
+    static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         return Console.ReadLine(); // Returns the user's name
     }
 
-    public static int PromptUserNumber()
+    static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
         return int.Parse(Console.ReadLine()); // Returns the user's favorite number
     }
 
-    public static int SquareNumber(int number)
+    static int SquareNumber(int number)
     {
         return number * number; // Returns the square of the number
     }
 
-    public static void DisplayResult(string name, int squaredNumber)
+    static void DisplayResult(string name, int squaredNumber)
     {
         Console.WriteLine($"{name}, the square of your number is {squaredNumber}.");
     }
 
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         DisplayWelcome(); // Display the welcome message
 
