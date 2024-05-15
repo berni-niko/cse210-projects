55a794e75fae7d0615fd636d4988d84047f6750e
berni.niko <berni.niko@hotmail.com>
2024-05-15 19:33:10 +1200

    Updated

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Linq.Expressions;
+using AssignmentApp.Assignment;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(writingAssignment.GetSummary());
+
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
