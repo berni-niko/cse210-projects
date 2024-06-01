ebe7a05dc25f2e8c220a0f16f26589a85d06a594
berni.niko <berni.niko@hotmail.com>
2024-06-01 19:18:12 +1200

    updated

--- final/Foundation3/Address.cs
+++ final/Foundation3/Address.cs
@@ -1,21 +1,21 @@
 public class Address
 {
-    private string Street;
-    private string City;
-    private string State;
-    private string Country;
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
 
     public Address(string street, string city, string state, string country)
     {
-        Street = street;
-        City = city;
-        State = state;
-        Country = country;
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
     }
 
 
     public string GetFullAddress()
     {
-        return $"{Street}\n{City}, {State}, {Country}";
+        return $"{_street}\n{_city}, {_state}, {_country}";
     }
 }
